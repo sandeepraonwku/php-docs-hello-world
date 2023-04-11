3d35e398eb0bff2a3c8a164229e8d8dc124d6f2b
sandeepraonwku <130413618+sandeepraonwku@users.noreply.github.com>
2023-04-10 21:39:06 -0400

    Docs.google.com url

--- postmain.php
+++ postmain.php
@@ -16,6 +16,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo $name;
   }
 }
+
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSfDFJTp7d9piY6RHUzBeCf5D3r_EfXcup-zy3bOs0azD-ojjQ/formResponse';
+$data = array('key1' => 'value1', 'key2' => 'value2');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
 ?>
 
 </body>
