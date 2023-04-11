06f29ceb12587004350f75bea51fc98e573c18c0
sandeepraonwku <130413618+sandeepraonwku@users.noreply.github.com>
2023-04-10 23:39:42 -0400

    get

--- postmain.php
+++ postmain.php
@@ -5,50 +5,20 @@
   Names: <input type="text" name="fname">
   <input type="submit">
 </form>
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $name = $_POST['fname'];
-  if (empty($name)) {
-    echo "Name is empty";
-  } else {
-    echo $name;
-  }
-}
+$url = "https://docs.google.com/forms/d/e/1FAIpQLSfDFJTp7d9piY6RHUzBeCf5D3r_EfXcup-zy3bOs0azD-ojjQ/formResponse?usp=pp_url&submit=Submit&entry.1412737491==234234242";
 
-$url = 'https://docs.google.com/spreadsheets/d/1NELcsymwPWRIYTULIuGyK50OPAQiGcpMYk-Vk9_ZVB0/formResponse';
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
+$resp = curl_exec($curl);
+curl_close($curl);
 
-//--------------------------------------------------------
-//The url you wish to send the POST request to
-//$url = $file_name;
-
-//The data you want to send via POST
-$fields = [
-    'entry.1412737491' => $name,
-     'btnSubmit'         => 'Submit'
-];
-
-//url-ify the data for the POST
-$fields_string = http_build_query($fields);
-
-//open connection
-$ch = curl_init();
-
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL, $url);
-curl_setopt($ch,CURLOPT_POST, true);
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-//So that curl_exec returns the contents of the cURL; rather than echoing it
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
-
-//execute post
-$result = curl_exec($ch);
-echo $result;
-
+echo $resp;
 ?>
+
+
 
 </body>
 </html>
