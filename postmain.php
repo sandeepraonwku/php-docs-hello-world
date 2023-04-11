4965039571ab639253b02342685b0eec5a8a8a04
sandeepraonwku <130413618+sandeepraonwku@users.noreply.github.com>
2023-04-11 14:33:34 -0400

    fix

--- postmain.php
+++ postmain.php
@@ -6,7 +6,7 @@
   <input type="submit">
 </form>
 <?php
-$url = "https://docs.google.com/forms/d/e/1FAIpQLSfDFJTp7d9piY6RHUzBeCf5D3r_EfXcup-zy3bOs0azD-ojjQ/formResponse?usp=pp_url&submit=Submit&entry.1412737491==234234242";
+$url = "https://docs.google.com/forms/d/e/1FAIpQLSfDFJTp7d9piY6RHUzBeCf5D3r_EfXcup-zy3bOs0azD-ojjQ/formResponse?usp=pp_url&submit=Submit&entry.1412737491=234234242";
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
