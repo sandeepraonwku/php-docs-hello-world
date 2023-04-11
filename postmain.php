90fdc45f3f2ce4dd379470ff74aab06ad8ad69e8
sandeepraonwku <130413618+sandeepraonwku@users.noreply.github.com>
2023-04-10 22:28:12 -0400

    update

--- postmain.php
+++ postmain.php
@@ -2,7 +2,7 @@
 <body>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
+  Names: <input type="text" name="fname">
   <input type="submit">
 </form>
 
